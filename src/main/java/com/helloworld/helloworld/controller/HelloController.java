daa3b1ea3953ffbc59071e7748bb7c7ac9f939ea
felipe <felipe_blade@hotmail.com>
2022-06-30 14:27:48 -0300

    Atualização do hello world com apenas um controller

--- src/main/java/com/helloworld/helloworld/controller/HelloController.java
+++ src/main/java/com/helloworld/helloworld/controller/HelloController.java
@@ -12,5 +12,19 @@ public class HelloController {
 	public String hello () {
     return "hello world";
 }
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "Comunicação,Persistência,Mentalidade de Crescimento,Proatividade,"
+				+ "Responsabilidade Pessoal,Atenção aos Detalhes, Trabalho em Equipe,"
+				+ "Orientação ao Futuro";
+	}
+	
+	@GetMapping("/objetivo")
+	public String objetivo() {
+		return "Participar do bootcamp,aprender novos conteudos,"
+	    		+ "fazer as atividades diárias,desenvolver o blog pessoal,"
+	    		+ "desenvolver o projeto integrador";
+	}
 }
+
 
